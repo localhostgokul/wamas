b9d86a796be339edd695817caae4bdaf2b6f12c5
gokul <localhostgokul@gmail.com>
2026-03-08 23:57:54 +0530

    feat: upgrade to .env file

--- API Server/config/db.config.js
+++ API Server/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url : "mongodb://localhost:27017/wamasif?retryWrites=true&w=majority"
+    // url : "mongodb://localhost:27017/wamasif?retryWrites=true&w=majority"
+    url : process.env.MONGODB_URI
 }
